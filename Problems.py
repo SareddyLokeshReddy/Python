4c6176d60a55c48bea5eacd3120c671fca4ea4a9
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-08 22:06:12 +0530

    reverse a number

--- Problems.py
+++ Problems.py
@@ -378,3 +378,10 @@ for i in range(n):
             print("*",end=" ")
     print() 
 #reverse a number
+n=int(input("Enter the number: "))
+rev =0
+while n>0:
+    d=n%10
+    rev=rev*10+d
+    n=n//10
+print(rev)
