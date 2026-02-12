567a9bf2b044ae4b049817692ed4a01b08868651
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-12 22:58:21 +0530

    pattern

--- Problems.py
+++ Problems.py
@@ -360,7 +360,7 @@ class Solution:
 #**
 #***
 #****
-n=int(input("Enter the number: "))
+r=int(input("Enter the number: "))
 for i in range(n):
     for j in range(i+1):
         print("*",end=" ")
