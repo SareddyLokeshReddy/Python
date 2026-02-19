0adbfc84254fe782b38f87e3848fa910d1a3a2ca
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-19 10:42:41 +0530

    reverse bits

--- Problems.py
+++ Problems.py
@@ -446,4 +446,12 @@ class Solution:
             return 2
         else:
             return 3
-#
+#reverse bits
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        rev = 0
+        for i in range(32):
+            rev <<= 1
+            rev |= (n & 1)
+            n >>= 1
+        return rev
