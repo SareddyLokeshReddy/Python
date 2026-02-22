fa938a653a3cf738f7f9398a6e2865150e206411
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-22 21:00:19 +0530

    pattern

--- patterns.py
+++ patterns.py
@@ -31,3 +31,9 @@ for i in range(1, n + 1):
 #AB
 #ABC
 #ABCD
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):           
+    print(" " * (n - i), end="")
+    for j in range(1, i + 1):
+        print(chr(64 + j), end="")
+    print()
