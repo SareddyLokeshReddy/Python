dab7430962b2a848320f681fb0b74c1ff2d1b90a
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-23 21:00:00 +0530

    Solved LeetCode problems

--- patterns.py
+++ patterns.py
@@ -21,8 +21,8 @@ for i in range(1, n + 1):
 #     12
 #.   123
 #.  1234
-n = int(input("Enter the number of rows: "))
-for i in range(1, n + 1):
+t = int(input("Enter the number of rows: "))
+for i in range(1, t + 1):
     print(" " * (n - i), end="")
     for j in range(1, i + 1):
         print(j, end="")
