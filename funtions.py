40ffe80d813f3b3514d82c9884e0453252e28c5a
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-02-28 21:08:13 +0530

    find mising elements

--- funtions.py
+++ funtions.py
@@ -321,3 +321,17 @@ class Solution:
                 count += value
         return count
 #find missing elements
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            index = abs(nums[i]) - 1
+            if nums[index] > 0:
+                nums[index] = -nums[index]
+
+        result = []
+        for i in range(n):
+            if nums[i] > 0:
+                result.append(i + 1)
+
+        return result
