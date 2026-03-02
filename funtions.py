90066bd650b0d2188614232eee7d16ac65bd5871
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-03-02 23:16:00 +0530

    ne

--- funtions.py
+++ funtions.py
@@ -345,4 +345,5 @@ n=int(input("Enter a number: "))
 sum=0
 for i in range(1,n+1):
     sum+=i
-print("The sum of first", n, "natural numbers is:", sum)    
+print("The sum of first", n, "natural numbers is:", sum)
+#chech whether the number is palindrome or not
