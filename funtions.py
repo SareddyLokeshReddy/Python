a1227a36eb2ebaa01dbf871b21f816330bfe2486
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-03-02 23:14:47 +0530

    first 10 natural numbers sum

--- funtions.py
+++ funtions.py
@@ -341,3 +341,8 @@ class Solution:
         s = s.split()
         return " ".join(s[:k])
 #first 10 natural numbers sum
+n=int(input("Enter a number: "))
+sum=0
+for i in range(1,n+1):
+    sum+=i
+print("The sum of first", n, "natural numbers is:", sum)    
