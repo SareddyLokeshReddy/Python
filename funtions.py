f13e3e56f93e3b619bba43680eb3a43dee2df730
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-03-05 00:00:47 +0530

    reverse words in a string III

--- funtions.py
+++ funtions.py
@@ -372,3 +372,10 @@ class Solution:
                 count += 1
         return count
 #reverse words in a strin III
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split(" ")
+        res = []
+        for w in words:
+            res.append(w[::-1])
+        return " ".join(res)
