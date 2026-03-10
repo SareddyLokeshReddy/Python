72d56b8679f3f5cd8d3abb83cb1d6f3ea9bce055
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-03-10 22:11:46 +0530

    new

--- funtions.py
+++ funtions.py
@@ -495,3 +495,8 @@ class Solution:
             return stack
         return remove(s) == remove(t)
 #find the difference of two arrays
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return [list(set1 - set2), list(set2 - set1)]
