24c244d522254dd588b367c8595a971a93373ef5
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-03-12 23:10:55 +0530

    new

--- funtions.py
+++ funtions.py
@@ -540,3 +540,14 @@ class Solution:
             res.append(s[i + k:i + 2 * k])
         return ''.join(res)
 #consequtive ones 
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        max_count = 0
+        count = 0
+        for num in nums:
+            if num == 1:
+                count += 1
+                max_count = max(max_count, count)
+            else:
+                count = 0
+        return max_count
