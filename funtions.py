2c85c97e90ae4c326b5d2ad6276449c769cfdbf9
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-03-13 23:21:34 +0530

    new

--- funtions.py
+++ funtions.py
@@ -575,3 +575,14 @@ class Solution:
         r.sort()
         return r[-1]*r[-2]
 #find the number of good pairs
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count={}
+        pairs=0
+        for num in nums:
+            if num in count:
+                pairs+=count[num]
+                count[num]+=1
+            else:
+                count[num]=1
+        return pairs
