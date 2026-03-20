f2e762531dca2c340f01b4fa52190c43606b7dbe
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-03-20 22:27:54 +0530

    new

--- funtions.py
+++ funtions.py
@@ -603,3 +603,8 @@ class Solution:
         r.sort()
         return r[-1]*r[-2]  
 #find the difference of two arrays
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return [list(set1 - set2), list(set2 - set1)]
