ae3bff338b44aa61b8edd9a321ba2bdb7b6065d4
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-03-29 10:44:25 +0530

    new

--- secony cycle.py
+++ secony cycle.py
@@ -12,4 +12,11 @@ def fac(n):
     return n+fac(n-1)
 print(fac(6))
 #fibanacci
+def fib(n):
+    if n==0:
+        return 0
+    if n==1 or n==2:
+        return 1
+    return fib(n-1)+fib(n-2)
+print(fib(5))
         
