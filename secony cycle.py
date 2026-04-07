e25a7851c30fe627de1f27e7beae574823d5d7e5
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-04-07 23:32:54 +0530

    new

--- secony cycle.py
+++ secony cycle.py
@@ -181,6 +181,19 @@ d = Dog()
 c = Cat()
 d.shout()   
 d.bark()    
-
 c.shout()  
 c.meow()   
+#abstract class
+from abc import ABC, abstractmethod         
+class Shape(ABC): 
+    @abstractmethod
+    def area(self):
+        pass
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+rect = Rectangle(5, 3)
+print("Area of the rectangle:", rect.area())        
