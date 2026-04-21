8b1a4ffd7f0eafb989ae684056fab073dc92f37d
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-04-21 22:59:02 +0530

    new

--- secony cycle.py
+++ secony cycle.py
@@ -316,3 +316,13 @@ class Solution:
         elif len(a) < t:
             return len(a)
 #employee details      
+class Employee:
+    def __init__(self, name, age, department):
+        self.name = name
+        self.age = age
+        self.department = department
+
+    def display_details(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Department: {self.department}")
