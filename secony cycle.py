fc0af58641c2ef2af479c5784df96067ff4047b3
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-04-22 22:46:48 +0530

    new

--- secony cycle.py
+++ secony cycle.py
@@ -326,3 +326,16 @@ class Employee:
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Department: {self.department}")
+#
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n==0 or n==1:
+            return n
+        if n==2:
+            return 1
+        T = [0] * (n+1)
+        T[0] = 0
+        T[1] = T[2] = 1
+        for i in range(3,n+1):
+            T[i] = T[i-1] + T[i-2] + T[i-3]
+        return T[n]
