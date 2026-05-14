859dbbea0b58759560e3d4d9672b88457954c658
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-05-14 12:32:37 +0530

    new

---
+++ filehandling.py
@@ -0,0 +1,18 @@
+f = open("file1.txt", "w")
+f.write("This is file handling in python")
+f.close()
+
+f=open("file1.txt","r")
+print(f.read())
+f.close()
+
+f=open("file1.txt","a")
+f.write("This is the python 3")
+f.close()
+
+f=open("file2.txt","x")
+f.close()
+
+f=open("file2.txt","w")
+f.write("This is the file handling in python 3")
+f.close()
