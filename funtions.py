c87f8c80c87545f522b8274c8a76339201e971f8
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-05-22 13:54:34 +0530

    new

--- funtions.py
+++ funtions.py
@@ -988,4 +988,12 @@ class Solution:
             s+=d
             p*=d
         return p-s
-        
+#max subarray
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        c=nums[0]
+        m=nums[0]
+        for i in range(1,len(nums)):
+            c=max(nums[i],c+nums[i])
+            m=max(m,c)
+        return m
