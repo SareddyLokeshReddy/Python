96189110b6ae9b348b39ea4f71fe4119e36195f4
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-05-24 21:55:34 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1035,3 +1035,13 @@ class Solution:
             if nums1[i] in nums2:
                 r.append(nums1[i])
         return list(set(r))
+#4
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        r=[]
+        for i in range(len(nums1)):
+            if nums1[i] in nums2:
+                r.append(nums1[i])
+                nums2.remove(nums1[i])
+        return list((r))
+        
