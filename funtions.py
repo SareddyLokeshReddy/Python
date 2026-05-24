7349b83c9bbaf664ef814d5de2215baa2409c5af
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-05-24 21:56:28 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1044,4 +1044,11 @@ class Solution:
                 r.append(nums1[i])
                 nums2.remove(nums1[i])
         return list((r))
-        
+#5
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        r=[]
+        for i in range(len(nums1)):
+            if nums1[i] in nums2:
+                r.append(nums1[i])
+        return list(set(r))
