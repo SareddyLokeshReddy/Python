789a4b0ed318b0e9d1bf81d30dfe2ac90a97577d
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-05-26 15:43:18 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1055,4 +1055,18 @@ class Solution:
 #class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         return arr.index(max(arr))
-        
+
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr) - 1
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if arr[mid] < arr[mid + 1]:
+                l = mid + 1
+            else:
+                r = mid
+
+        return l
