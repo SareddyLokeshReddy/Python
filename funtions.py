92239704fd998280e1c474570d3811382a87cf93
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-05-29 20:27:33 +0530

    matrix

--- funtions.py
+++ funtions.py
@@ -1121,3 +1121,19 @@ for i in range(len(A)):
     res.append(row)
 
 print(res)
+#substraction
+A = [[1, 2],
+     [3, 4]]
+
+B = [[5, 6],
+     [7, 8]]
+
+res = []
+
+for i in range(len(A)):
+    row = []
+    for j in range(len(A[0])):
+        row.append(A[i][j] - B[i][j])
+    res.append(row)
+
+print(res)
