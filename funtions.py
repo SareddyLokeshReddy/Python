4f171dd6d16a495354fbb4f96d7cc22ce19a0332
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-05-30 22:16:44 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1145,3 +1145,13 @@ class Solution:
                 t += int(j)
             nums[i] = t
         return min(nums)
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for row in image:
+            left = 0
+            right = len(row) - 1
+            while left <= right:
+                row[left],row[right] = 1 - row[right], 1- row[left]
+                left += 1
+                right -= 1
+        return image
