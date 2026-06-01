019a74204a982e321fd1f4001511aac2af683018
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-06-01 22:00:42 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1183,6 +1183,14 @@ class Solution:
                 sum+=accounts[i][j]
             s.append(sum)
         return max(s)
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        c=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]<0:
+                    c+=1
+        return c
         
         
         
