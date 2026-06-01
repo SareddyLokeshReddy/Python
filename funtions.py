725b01dfc34b3654a7b6ec756b8d8269e8932ab6
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-06-01 22:12:27 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1191,6 +1191,14 @@ class Solution:
                 if grid[i][j]<0:
                     c+=1
         return c
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        c=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]<0:
+                    c+=1
+        return c
         
         
         
