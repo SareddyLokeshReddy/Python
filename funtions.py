ad27f3c3653a960e832c1f12246338687a7a4b32
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-06-03 22:53:28 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1211,7 +1211,20 @@ class Solution:
             if c==len(matrix)-1:
                 r.append(m)
         return r
-        
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l=0
+        r=len(nums) - 1
+        while l<=r:
+            m = (l+r)//2
+            if nums[m]==target:
+                return m
+            elif nums[m]<target:
+                l=m+1
+            else:
+                r=m-1
+        return -1
+
         
 
  
