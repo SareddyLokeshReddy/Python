6964b3c10256b47f6dd4ea96bf952eddcc4c31fd
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-06-04 14:43:14 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1225,6 +1225,20 @@ class Solution:
                 r=m-1
         return -1
 
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l = 1
+        r = n
+        
+        while l <= r:
+            m = (l + r) // 2
+            res = guess(m)
+            if res == 0:
+                return m
+            elif res == -1:
+                r = m - 1
+            else:
+                l = m + 1
         
 
  
