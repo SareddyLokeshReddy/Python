fb3e1597e469f155cf9e5ee85c993065b1ffc885
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-06-10 00:00:58 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1310,9 +1310,9 @@ def plusMinus(arr):
     print(z/n)
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        n = len(nums)
-        for i in range(n):
-            for j in range(n - i - 1):
+        num = len(nums)
+        for i in range(num):
+            for j in range(num - i - 1):
                 if nums[j] > nums[j + 1]:
                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
         
