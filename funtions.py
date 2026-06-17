1ed4831589aa0bddc7557fc3927eacb93fc493db
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-06-17 23:17:56 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1417,5 +1417,9 @@ class Solution:
             else:
                 c+=5
         return c
+class Solution:
+    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+        return min(nums2)-min(nums1)
+        
 
  
