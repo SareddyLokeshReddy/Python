ac485cd33f9b6f35c01abed2903f62febcabc70c
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-06-28 15:32:39 +0530

    array sum

--- patterns.py
+++ patterns.py
@@ -47,3 +47,8 @@ for i in range(1, n + 1):
     for j in range(1, i + 1):
         print(chr(64 + j), end="")
     print()
+def simpleArraySum(ar):
+    s=0
+    for i in ar:
+        s+=i
+    return s
