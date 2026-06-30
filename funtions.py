b291a0241445eb1c9eb6e53701adb98e83e749f7
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-06-30 22:39:54 +0530

    count the substrings words

--- funtions.py
+++ funtions.py
@@ -1537,7 +1537,14 @@ class Solution:
             for j in str(i):
                 l.append(int(j))
         return l
-
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        count=0
+        for i in patterns:
+            if i in word:
+                count+=1
+        return count
+        
         
 
 
