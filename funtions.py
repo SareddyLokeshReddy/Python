796b0e6542f3a4d12eab0380be60e081d7de324d
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-07-06 22:10:05 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1551,7 +1551,30 @@ def divisibleSumPairs(n, k, ar):
             if (ar[i] + ar[j]) % k == 0:
                 c += 1
     return c
+def isPrime(n): 
+    if n < 2: 
+        return False 
+    for i in range(2, n): 
+        if n % i == 0: 
+            return False 
+    return True
         
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        if s == goal:
+            return len(set(s)) < len(s)
+        diff = []
+        for i in range(len(s)):
+            if s[i] != goal[i]:
+                diff.append(i)
+        if len(diff) != 2:
+            return False
+        i, j = diff
+        return s[i] == goal[j] and s[j] == goal[i]
+
+
         
 
 
