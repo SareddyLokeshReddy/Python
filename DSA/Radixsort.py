2abf922414ce4b011a751ba9cab90a588d927081
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-07-15 22:30:17 +0530

    new

--- DSA/Radixsort.py
+++ DSA/Radixsort.py
@@ -28,3 +28,17 @@ print(b)
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
         return n
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        while left < right:
+            width = right - left
+            current_area = width * min(height[left], height[right])
+            max_area = max(max_area, current_area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
