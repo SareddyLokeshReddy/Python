63a1d406b09f6ed87441e19c80a91ae97b035b73
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-07-16 23:45:09 +0530

    new

--- DSA/Radixsort.py
+++ DSA/Radixsort.py
@@ -42,3 +42,13 @@ class Solution:
             else:
                 right -= 1
         return max_area
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sp = tp = 0
+
+        while sp < len(s) and tp < len(t):
+            if s[sp] == t[tp]:
+                sp += 1
+            tp += 1
+        
+        return sp == len(s)
