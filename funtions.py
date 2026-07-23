a49e76f6b3d4a92db198fef693f61e88464cf2ad
Lokesh Reddy <sareddylokeshreddy@gmail.com>
2026-07-23 21:27:09 +0530

    new

--- funtions.py
+++ funtions.py
@@ -1620,7 +1620,16 @@ class Solution:
             if freq[word] == 1:
                 ans.append(word)
         return ans
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sp = tp = 0
+
+        while sp < len(s) and tp < len(t):
+            if s[sp] == t[tp]:
+                sp += 1
+            tp += 1
         
+        return sp == len(s)
 
         
         
